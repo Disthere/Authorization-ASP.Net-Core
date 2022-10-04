3411fe690934c42ff3537713a8895ed07642a77c
Вячеслав Литвиненко <ptb.ikc@yandex.ru>
2022-10-04 14:34:09 +0500

    Добавьте файлы проекта.

---
+++ Authorization ASP.Net Core.Basics 5.0/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization_ASP.Net_Core.Basics_5._0.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Authorization ASP.Net Core.Basics 5.0/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization_ASP.Net_Core.Basics_5._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
