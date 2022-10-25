4f64c438e099e845edf4b6530a45d09d76ff0e85
Вячеслав Литвиненко <ptb.ikc@yandex.ru>
2022-10-25 11:37:11 +0500

    Identity Server

---
+++ Identity.IdentityServer/Controllers/SiteController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Identity.IdentityServer.Controllers
+{
+    [Route("[controller]")]
+    public class SiteController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SiteController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public async Task<IActionResult> GetOrders()
+        {
+            var ordersClient = _httpClientFactory.CreateClient();
+
+            var responce = await ordersClient.GetAsync("https://localhost:7001/site/GetSecrets");
+
+            var message = await responce.Content.ReadAsStringAsync();
+
+            ViewBag.Message = message;
+
+            return View();
+        }
+
+    }
+}
---
+++ Identity.Orders.WebApi/Controllers/SiteController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.IdentityServer.WebApi.Controllers
+{
+    [Route("[controller]")]
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public string GetSecrets()
+        {
+            return "Secret string from Orders.WebApi";
+        }
+    }
+}
